699aff18668f05cf4d53a2332ce557997c328d2c
sandrotimmy <smachado.ti@gmail.com>
2017-05-28 23:38:45 -0300

    configurado conexao com banco de dados mySql e adicionado o banco protocoloDB no projeto, iniciado configuração do webService

--- Pacotes de Codigo fonte/br/com/MVC/controllers/ConexaoEntityManager.java
+++ Pacotes de Codigo fonte/br/com/MVC/controllers/ConexaoEntityManager.java
@@ -1,5 +1,8 @@
 package br.com.MVC.controllers;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -7,16 +10,12 @@ import javax.persistence.Persistence;
 
 public class ConexaoEntityManager {
 
-    private static EntityManager em;
-    private static EntityManagerFactory emf;
-
-    public static EntityManager getInstance() {
-        if (em == null) {
-            emf = Persistence.createEntityManagerFactory("protocoloDB");
-            em = emf.createEntityManager();
-        } else {
-            em = emf.createEntityManager();
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(
+          "jdbc:mysql://localhost/protocoloDB", "root", "masterkey");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-        return em;
     }
 }
